f9a5ddc3c7fccd4ef5c42c9b4f0543104f2b20d6
Grigoriy <grishayugov@mail.ru>
2024-02-27 20:03:45 +0300

    Initial commit

---
+++ src/test/java/ru/edu/effectivemobile/EffectiveMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.effectivemobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
